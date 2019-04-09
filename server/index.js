0a6d3a8a014ebe5204ce97f7bab76387c44bf4e4
amarachukwu-agbo <amarachukwu.agbo@andela.com>
2019-04-09 22:20:06 +0100

    chore(eslint): set up eslint
    - install eslint and peer dependencies
    - create eslint config file
    
    [Delivers 165145036]

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`); // eslint-disable-line
 });
 
 export default app;
